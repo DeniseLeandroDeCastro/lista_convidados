b73c6af38e4c63c692bc864a6bcd8cb0d3e95a14
Denise Castro <dmaria.natal@gmail.com>
2022-01-02 16:55:16 -0300

    app para lista de convidados para sua festa

---
+++ app/src/main/java/com/example/convidados/service/model/GuestModel.kt
@@ -0,0 +1,23 @@
+package com.example.convidados.service.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity Class (the model is created thinking about what will be saved in the database).
+ */
+
+@Entity(tableName = "Guest")
+class GuestModel {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = true
+}
